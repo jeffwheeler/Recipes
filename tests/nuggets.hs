ebb0fcd2fbfe8bf94ddeeefbc39947fb9ab51c98
= <wheele11@illinois.edu>
2010-01-23 17:45:12 -0600

    Properly print errors

--- tests/nuggets.hs
+++ tests/nuggets.hs
@@ -6,6 +6,6 @@ import Data.Recipe.Structure
 main = do
   p <- parseFile "tests/alfredo.recipe"
   case p of
-    Left err -> print err
+    Left err -> putStrLn err
     Right r  -> writeFile "/home/jeff/Desktop/recipe.html" (recipeGraph r)
 
